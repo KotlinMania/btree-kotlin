db864f716f06ce58d1637bd468163f9752cce7c3
Sydney Renee <sydney@solace.ofharmony.ai>
2026-05-05 12:52:57 -0700

    Fix publish config + remove macosX64

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 import org.gradle.language.base.plugins.LifecycleBasePlugin
 import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFramework
+import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnRootExtension
 
 plugins {
     kotlin("multiplatform") version "2.3.21"
@@ -46,6 +47,12 @@ kotlin {
     val xcf = XCFramework("BTreeKotlin")
 
     macosArm64 {
+        binaries.framework {
+            baseName = "BTreeKotlin"
+            xcf.add(this)
+        }
+    }
+    macosX64 {
         binaries.framework {
             baseName = "BTreeKotlin"
             xcf.add(this)
@@ -102,6 +109,12 @@ kotlin {
         val commonTest by getting { dependencies { implementation(kotlin("test")) } }
     }
     jvmToolchain(21)
+}
+
+rootProject.extensions.configure<YarnRootExtension>("kotlinYarn") {
+    resolution("diff", "8.0.3")
+    resolution("serialize-javascript", "7.0.5")
+    resolution("webpack", "5.106.2")
 }
 
 tasks.register("test") {
